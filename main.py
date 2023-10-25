c78d92bfcd45b5ba6854e651430757f9ae5f0ffa
Rodrigo Oliveira <rodrikpoliveira@gmail.com>
2023-10-25 19:12:18 -0300

    Added feature to download from txt file

--- main.py
+++ main.py
@@ -1,24 +1,47 @@
 import argparse
 import helpers
 
-parser = argparse.ArgumentParser()
 
-parser.add_argument("url", help="4Chan thread URL")
-parser.add_argument(
-    "-o", "--output", help="Output path", default="./downloads"
-)
-# TODO: Download image only or video only mode
-# TODO: Download from url in txt file
+def main():
+    parser = argparse.ArgumentParser()
 
-args = parser.parse_args()
+    url_group = parser.add_mutually_exclusive_group()
+    url_group.add_argument(
+        "-u", "--url", help="4Chan thread URL"
+    )
+    url_group.add_argument(
+        "-f", "--file", help="File with multiple urls"
+    )
 
-url = args.url
-download_dir = args.output + f"/{helpers.thread_name(url)}"
+    parser.add_argument(
+        "-o", "--output", help="Output path", default="./downloads"
+    )
+    # TODO: Download image only or video only mode
 
-helpers.check_dir(download_dir)
+    args = parser.parse_args()
 
-for url, name, ext in helpers.get_media(url):
-    print(f"Downloading {name}.{ext}")
-    helpers.download(url, name, ext, download_dir)
+    if not args.url and not args.file:
+        return print("Must select either --file or --url.")
 
-print("All media downloaded.")
+    if args.file:
+        with open(args.file, "r") as f:
+            for url in f:
+                download(url, args.output)
+    else:
+        download(args.url, args.output)
+
+
+def download(url: str, download_dir: str):
+    download_dir += f"/{helpers.thread_name(url)}"
+
+    helpers.check_dir(download_dir)
+
+    for url, name, ext in helpers.get_media(url):
+        print(f"Downloading {name}.{ext}")
+        helpers.download_media(url, name, ext, download_dir)
+
+    print("All media downloaded.")
+
+
+if __name__ == "__main__":
+    main()
